fab5cf94ba032ab54a36785cb79599144b6d1e5e
Eric Keith <ericwkeith@gmail.com>
2019-12-16 08:58:21 -0600

    button ID's and colors

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
+// myWindow = window.open("", "", "width=320, height=280");
+
 let clicks = 0;
 
 function myFunction (note) {
-  navigator.clipboard.writeText(note)
   document.body.style.backgroundImage = "url('images/leftPaw.png')"
   clicks ++;
   document.getElementById('clicks').innerHTML = clicks;
+  navigator.clipboard.writeText(note)
 }
 
 function myFunction2 (note) {
-  navigator.clipboard.writeText(note)
   document.body.style.backgroundImage = "url('images/rightPaw.png')"
   clicks ++;
   document.getElementById('clicks').innerHTML = clicks;
+  navigator.clipboard.writeText(note)
 }
