253d74c32a6ac95e95f1f882d7dec678f53938c4
Eric Keith <ericwkeith@gmail.com>
2019-12-17 14:10:07 -0600

    changed how the background is updated

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
-// myWindow = window.open("", "", "width=320, height=280");
-
 let clicks = 0;
 
 function myFunction (note) {
-  document.body.style.backgroundImage = "url('images/leftPaw.png')"
   clicks ++;
   document.getElementById('clicks').innerHTML = clicks;
-  navigator.clipboard.writeText(note)
+  navigator.clipboard.writeText(note);
+  isEven(clicks);
 }
 
-function myFunction2 (note) {
-  document.body.style.backgroundImage = "url('images/rightPaw.png')"
-  clicks ++;
-  document.getElementById('clicks').innerHTML = clicks;
-  navigator.clipboard.writeText(note)
+function isEven(value) {
+	if (value%2 == 0)
+    document.body.style.backgroundImage = "url('images/rightPaw.png')";
+	else
+    document.body.style.backgroundImage = "url('images/leftPaw.png')";
 }
+
